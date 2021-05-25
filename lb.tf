dda579abf45d9db87a893891af35c966cd7caf83
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2021-05-25 22:08:54 +0100

    add nlb to peering proxy

--- lb.tf
+++ lb.tf
@@ -43,7 +43,7 @@ resource "aws_lb" "peering-proxy" {
   security_groups            = [aws_security_group.nlb.id]
   enable_deletion_protection = true
   subnet_mapping {
-    subnet_id = [aws_subnet.haproxy_subnet.id]
+    subnet_id = aws_subnet.haproxy_subnet.id
     # private_ipv4_address  = var.haproxy_private_ip\
     private_ipv4_address = "10.3.0.13"
   }
