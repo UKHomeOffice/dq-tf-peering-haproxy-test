260f0725d2c90a62b8b9cb9d5ce4de8c396bbc98
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2021-05-25 22:05:21 +0100

    add nlb to peering proxy

--- asg.tf
+++ asg.tf
@@ -5,7 +5,7 @@ resource "aws_autoscaling_group" "peering-proxy" {
   launch_configuration      = aws_launch_configuration.peering-proxy.id
   min_size                  = 1
   max_size                  = 2
-  load_balancers            = [aws_elb.peering-proxy.name]
+  load_balancers            = [aws_lb.peering-proxy.name]
   health_check_type         = "ELB"
   health_check_grace_period = 300
   vpc_zone_identifier       = [aws_subnet.haproxy_subnet.id]
