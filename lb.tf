0a6f1a0cb2d8a64a8386c354f8f4e6daf7aa120c
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2021-05-25 21:54:41 +0100

    add nlb to peering proxy

---
+++ lb.tf
@@ -0,0 +1,91 @@
+
+
+variable "forwarding_config" {
+  default = {
+    5431 = "TCP"
+    5432 = "TCP"
+    8080 = "TCP"
+    8081 = "TCP"
+    8082 = "TCP"
+    8083 = "TCP"
+    8084 = "TCP"
+    8085 = "TCP"
+    8086 = "TCP"
+    8087 = "TCP"
+  }
+}
+
+## Security Group for ELB
+resource "aws_security_group" "nlb" {
+  name = "nlb-sg-${local.naming_suffix}"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = [var.haproxy_subnet_cidr_block]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8090
+    protocol    = "tcp"
+    cidr_blocks = var.SGCIDRs
+  }
+  ingress {
+    from_port   = 5431
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = var.SGCIDRs
+  }
+}
+
+resource "aws_lb" "peering-proxy" {
+  name                       = "nlb-${local.naming_suffix}"
+  security_groups            = [aws_security_group.nlb.id]
+  enable_deletion_protection = true
+  subnet_mapping {
+    subnet_id = [aws_subnet.haproxy_subnet.id]
+    # private_ipv4_address  = var.haproxy_private_ip\
+    private_ipv4_address = "10.3.0.13"
+  }
+  tags = {
+    Name = "nlb-${local.naming_suffix}"
+  }
+}
+
+resource "aws_lb_listener" "peering-proxy" {
+  load_balancer_arn = aws_lb.peering-proxy.arn
+  for_each          = var.forwarding_config
+  port              = each.key
+  protocol          = each.value
+  default_action {
+    target_group_arn = aws_lb_target_group.peering-proxy[each.key].arn
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_target_group" "peering-proxy" {
+  for_each             = var.forwarding_config
+  name                 = "peering-proxy-tg-${each.key}"
+  port                 = each.key
+  protocol             = each.value
+  vpc_id               = var.peeringvpc_id
+  target_type          = "instance"
+  deregistration_delay = 90
+  health_check {
+    interval            = 60
+    port                = each.value
+    protocol            = "TCP"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+  tags = {
+    Environment = "peering-proxy-tg-${each.key}"
+  }
+}
+
+# resource "aws_lb_target_group_attachment" "peering-proxy" {
+#   for_each         = var.forwarding_config
+#   target_group_arn = aws_lb_target_group.peering-proxy[each.key].arn
+#   port             = each.key
+#   target_id        = lookup(var.tg_config, "target_id1")
+# }
