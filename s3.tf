1b8fe8eb73b396e7d783479af946cf469bb9f2f2
Blake McKenzie <154533953+blake-mck-HO@users.noreply.github.com>
2024-03-06 12:48:43 +0000

    replaced deprecated logging block with logging resource as prompted by tf warning and highlighted in hashicorp docs

--- s3.tf
+++ s3.tf
@@ -21,10 +21,10 @@ resource "aws_s3_bucket" "haproxy_config_bucket" {
   #  enabled = true
   #  }
 
-  logging {
-    target_bucket = var.log_archive_s3_bucket
-    target_prefix = "${var.service}-log/"
-  }
+  # logging {
+  #  target_bucket = var.log_archive_s3_bucket
+  #  target_prefix = "${var.service}-log/"
+  # }
 
   tags = {
     Name = "s3-${local.naming_suffix}"
@@ -36,6 +36,13 @@ resource "aws_s3_bucket_versioning" "haproxy_config_bucket_versioning" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket_logging" "haproxy_config_bucket_logging" {
+  bucket = var.s3_bucket_name
+
+  target_bucket = var.log_archive_s3_bucket
+  target_prefix = "${var.service}-log/"
 }
 
 resource "aws_s3_bucket_policy" "haproxy_config_bucket" {
