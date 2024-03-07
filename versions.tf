d7a7f06a0acdae6dc1465f936bf8359e2a58d29e
Blake McKenzie <154533953+blake-mck-HO@users.noreply.github.com>
2024-03-07 12:30:24 +0000

    readded required providers to resolve drone testrunner step error

--- versions.tf
+++ versions.tf
@@ -2,8 +2,14 @@
 terraform {
   required_version = "1.5"
   required_providers {
+    archive = {
+      source = "hashicorp/archive"
+    }
     aws = {
       source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
     }
   }
 }
